6cd980bd2090bee3eb9184783db5ca30fbfb3edf
Carter Tsai <hamming1@gmail.com>
2013-11-05 23:34:59 +0800

    Add Multiple & Named Views example

--- ui-router/app/scripts/app.js
+++ ui-router/app/scripts/app.js
@@ -3,15 +3,30 @@
 angular.module('uiRouterApp', [
   'ngCookies',
   'ngResource',
-  'ngSanitize'
+  'ngSanitize',
+  'ui.router'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/home");
+
+    $stateProvider
+        .state('contacts', {
+            url: "/home",
+            templateUrl: "views/main.html",
+            controller:  "MainCtrl"
+        })
+        .state('detail', {
+            url: "/detail",
+            views: {
+                "": {templateUrl: "views/main.html"},
+                "detail": {
+                    template: "<div> detail </div>",
+                    controller: function() { console.log("123") }
+                },
+                "list": {
+                    template: "<div> list </div>",
+                    controller: function() { console.log("123") }
+                }
+            }
+        })
   });
